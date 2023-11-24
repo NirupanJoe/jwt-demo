18e6a5cedc531e2b54505f87609622696affce6e
njj-abs <njj@abstractit.in>
2023-11-24 20:03:14 +0530

    Verified the token.

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/login', (req, res) => {
 	const user = users.find(({ userName }) => userName === req.body.userName);
-
 	if (user) {
 		if (user.password === req.body.password) {
 			const token = jwt.sign({ userID: user.id }, 'secret');
@@ -27,3 +26,33 @@ app.post('/login', (req, res) => {
 	}
 })
 
+const checkToken = (req, res, next) => {
+	const token = req.headers["authorization"];
+	console.log(req.headers, req.headers["a"]);
+	if (token) {
+		jwt.verify(token, 'secret', (err, decoded) => {
+			if (err) {
+				res.status(401).send(err)
+
+				return
+			}
+			else {
+				req.userID = decoded.userID
+				next()
+			}
+		})
+	} else {
+		res.status(401).send({ message: 'Access Denied' })
+	}
+}
+
+app.get("/data", checkToken, (req, res) => {
+	const filteredCars = cars.filter((car) => car.userId === req.userID)
+
+	res.status(200).send({ data: filteredCars })
+})
+
+
+app.listen(port, () => {
+	console.log("Running post" + port);
+})
